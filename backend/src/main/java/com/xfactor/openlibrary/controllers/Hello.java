ff660567accf15f895800399d839c38f3c6d74f1
Deep Goenka <deepgoenkamail@gmail.com>
2023-04-01 23:53:16 +0530

    first commit

--- backend/src/main/java/com/xfactor/openlibrary/controllers/Hello.java
+++ backend/src/main/java/com/xfactor/openlibrary/controllers/Hello.java
@@ -1,14 +1,5 @@
 package com.xfactor.openlibrary.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class Hello {
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!!";
-    }
 
 }
